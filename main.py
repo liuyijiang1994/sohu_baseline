32108582f562d757dfd68a8d09e2ca40031cb000
liu <549270875@qq.com>
2019-04-10 20:30:37 +0800

    目录修改

---
+++ main.py
@@ -0,0 +1,35 @@
+from net.bert_ner import Bert_CRF
+from data_loader import create_batch_iter
+from train import fit
+import args as args
+from util.porgress_util import ProgressBar
+from data_processor import produce_data
+
+
+def start():
+    produce_data()
+    train_iter, num_train_steps = create_batch_iter("train")
+    eval_iter = create_batch_iter("dev")
+
+    epoch_size = num_train_steps * args.train_batch_size * args.gradient_accumulation_steps / args.num_train_epochs
+
+    pbar = ProgressBar(epoch_size=epoch_size, batch_size=args.train_batch_size)
+
+    model = Bert_CRF.from_pretrained(args.bert_model,
+                                     num_tag=len(args.labels))
+
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(name)
+
+    fit(model=model,
+        training_iter=train_iter,
+        eval_iter=eval_iter,
+        num_epoch=args.num_train_epochs,
+        pbar=pbar,
+        num_train_steps=num_train_steps,
+        verbose=1)
+
+
+if __name__ == '__main__':
+    start()
